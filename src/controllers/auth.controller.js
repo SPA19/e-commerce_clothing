09394f60fe45d82e0d5ea4f5db332130d82ae9b8
Simón Posada Acosta <simon.150@hotmail.com>
2023-11-24 20:38:36 -0500

    Se crearon rutas y controladores

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -40,7 +40,8 @@ export const login = async (req, res) => {
 
     const isMatch = await bcryp.compare(password, userFound.password);
 
-    if (!isMatch) return res.status(400).json({ message:"Incorrect Password"})
+    if (!isMatch)
+      return res.status(400).json({ message: "Incorrect Password" });
 
     const token = await createAccessToken({ id: userFound._id });
 
@@ -55,4 +56,24 @@ export const login = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+};
+
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
+};
+
+export const profile = async (req, res) => {
+  const userFound = await User.findById(req.user.id);
+  if (!userFound) return res.status(400).json({ message: "user not found" });
+
+  return res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+    createdAt: userFound.createdAt,
+    updatedAt: userFound.updatedAt,
+  });
 };
